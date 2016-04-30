f13ee96435d7803e838b2ac3572f547040da504c
Luca Sbardella <luca@quantmind.com>
2016-04-30 13:30:15 +0100

    use requests as default client

--- fluidily/client.py
+++ fluidily/client.py
@@ -8,8 +8,8 @@ class Fluidily:
 
     def __init__(self, url=None, sessions=None, token=None):
         if sessions is None:
-            from pulsar.apps.http import HttpClient
-            sessions = HttpClient()
+            import requests
+            sessions = requests.Session()
         self.url = url or self.url
         self.token = token
         self.sessions = sessions
@@ -28,7 +28,8 @@ class Fluidily:
         token = self.token
         self.token = None
         try:
-            data = self.execute('/authorizations', 'post', data=params)
+            data = self.execute(self.url + '/authorizations',
+                                'post', data=params)
         except Exception:
             self.token = token
             raise
@@ -37,31 +38,31 @@ class Fluidily:
 
     def execute(self, url, method=None, headers=None, **params):
         method = method or 'GET'
-        headers = headers or []
+        headers = headers or {}
         if self.token:
-            headers.append(('Authorization', 'Bearer %s' % self.token))
-        headers.append(('content-type', 'application/json'))
-        headers.append(('accept', 'application/json, text/*; q=0.5'))
+            headers['Authorization'] = 'Bearer %s' % self.token
+        headers['Content-Type'] = 'application/json'
+        headers['Accept'] = 'application/json, text/*; q=0.5'
         response = self.sessions.request(method, url, headers=headers,
                                          **params)
         if not response.status_code:
             response.raise_for_status()
         elif response.status_code >= 400:
-            error = response.decode_content()
-            if isinstance(error, dict):
-                error = error.get('message', error)
-            error = '%d: %s' % (response.status_code, error)
-            raise FluidilyError(status=response.status_code, msg=error)
+            try:
+                error = response.json()
+            except Exception:
+                error = response.text
+            raise FluidilyError(response.status_code, error)
         if response.raise_for_status == 204:
             return True
-        return response.decode_content()
+        return response.json()
 
 
 class FluidilyError(Exception):
 
-    def __init__(self, status, msg):
-        super().__init__(msg)
+    def __init__(self, status, error):
         self.status_code = status
+        self.error = error
 
 
 class Fluid:
@@ -81,7 +82,7 @@ class Fluid:
         return self.root.execute('%s/%s' % (self.url, id))
 
     def create(self, **params):
-        return self.root.execute(self.url, 'post', data=params)
+        return self.root.execute(self.url, 'post', json=params)
 
     def delete(self, id):
         return self.root.execute('%s/%s' % (self.url, id), 'delete')
--- tests.py
+++ tests.py
@@ -1,32 +1,44 @@
 import os
-import json
 import unittest
-from unittest import skipUnless
 
-import fluidily
+from fluidily import Fluidily, FluidilyError
+
 
 try:
     import fluidily_config  # noqa
 except ImportError:
     pass
 
-
-cfg = json.loads(os.environ.get('FLUIDILY_CONFIG', '{}'))
-
-client = fluidily.Fluidily(cfg.pop('url', 'https://api.quantmind.com'),
-                           token=cfg.pop('token', None))
+test_app = 'fluid-test'
+test_organisation = os.environ.get('FLUIDILY_ORGANISATION')
+client = Fluidily(os.environ.get('FLUIDILY_URL'),
+                  token=os.environ.get('FLUIDILY_TOKEN'))
 
 
 class TestFluidily(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        try:
+            client.applications.delete(test_app)
+        except FluidilyError:
+            pass
+        client.applications.create(name=test_app,
+                                   organisation=test_organisation)
+
+    @classmethod
+    def tearDownClass(cls):
+        client.applications.delete(test_app)
+
     def test_urls(self):
         self.assertTrue(client.urls())
-
-    @skipUnless(cfg.get('username'), 'requires username')
-    def test_token(self):
-        token = client.get_token(**cfg)
-        self.assertEqual(client.token, token)
 
     def test_applications_get_list(self):
         apps = client.applications.get_list()
         self.assertTrue(apps)
+
+    # TEMPLATES
+    def test_create_fail(self):
+        with self.assertRaises(FluidilyError) as e:
+            client.templates.create(name='test', body='{{ html_main }}')
+        self.assertEqual(e.exception.status_code, 422)
